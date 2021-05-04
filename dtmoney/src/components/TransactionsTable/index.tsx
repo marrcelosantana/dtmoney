7bdc8784212a00e8ce48714bd1803292d3660cf5
marrcelosantana <marcelosantana@alu.ufc.br>
2021-05-04 03:15:11 -0300

    First Commit

---
+++ dtmoney/src/components/TransactionsTable/index.tsx
@@ -0,0 +1,42 @@
+import { Container } from "./styles";
+import { useEffect } from "react";
+import { api } from "../../services/api";
+
+export function TransactionsTable(){
+
+    useEffect(() => {
+        api.get('transactions')
+        .then(response => console.log(response.data))
+    }, []);
+
+    return (
+        <Container>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Título</th>
+                        <th>Valor</th>
+                        <th>Categoria</th>
+                        <th>Data</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <tr>
+                        <td className = "title">Desenvolvimento de website</td>
+                        <td className = "deposit">R$12.000,00</td>
+                        <td>Desenvolvimento</td>
+                        <td>20/01/2021</td>
+                    </tr>
+                    <tr>
+                        <td className = "title">Aluguel</td>
+                        <td className = "withdraw">- R$1.100,00</td>
+                        <td>Casa</td>
+                        <td>17/01/2021</td>
+                    </tr>
+                   
+                </tbody>
+            </table>
+        </Container>
+    );
+}
