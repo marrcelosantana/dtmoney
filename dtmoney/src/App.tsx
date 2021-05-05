7d358effb18fc92fb8be36cf57aff28a467180fc
marrcelosantana <marcelosantana@alu.ufc.br>
2021-05-05 03:57:41 -0300

    Att

--- dtmoney/src/App.tsx
+++ dtmoney/src/App.tsx
@@ -1,12 +1,31 @@
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
+import Modal from 'react-modal';
+import { useState } from 'react';
+import { NewTransactionModal } from "./components/NewTransactionModal";
+
+Modal.setAppElement('#root');
 
 export function App() {
+
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+    function handleOpenNewTransactionModal(){
+        setIsNewTransactionModalOpen(true);
+    }
+
+    function handleCloseNewTransactionModal(){
+        setIsNewTransactionModalOpen(false);
+    }
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal = {handleOpenNewTransactionModal} />
       <Dashboard />
+
+      <NewTransactionModal isOpen = { isNewTransactionModalOpen } onRequestClose = { handleCloseNewTransactionModal }/>
+
       <GlobalStyle />
     </>
   );
---
+++ dtmoney/src/components/NewTransactionModal/index.tsx
@@ -0,0 +1,42 @@
+import Modal from 'react-modal';
+import { Container } from './styles';
+import closeImg from '../../assets/close.svg';
+
+interface NewTransactionModalProps {
+    isOpen: boolean;
+    onRequestClose: () => void;
+}
+
+export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps){
+    return (
+        <Modal isOpen = { isOpen } 
+            onRequestClose = { onRequestClose } 
+            overlayClassName = "react-modal-overlay"
+            className = "react-modal-content"
+        >
+
+            <button type = "button" onClick = { onRequestClose } className = "react-modal-close">
+                <img src= { closeImg } alt="Fechar modal"/>
+            </button>
+
+            <Container>
+                <h2>Cadastrar Transação</h2>
+
+                <input placeholder = "Título"/>
+
+                <input type = "number" placeholder = "Preço"/>
+
+                <input placeholder = "Categoria"/>
+
+                <button type="submit">
+                    Cadastrar
+                </button>
+
+
+
+            </Container>
+            
+        </Modal>
+
+    );
+}
